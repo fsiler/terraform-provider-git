eba8526709f89632429456478082f000c9f10e38
Frank Siler <me@franksiler.com>
2024-04-11 15:13:48 -0500

    Update main.go

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/innovationnorway/terraform-provider-git/internal/git"
+	"github.com/fsiler/terraform-provider-git/internal/git"
 )
 
 func main() {
